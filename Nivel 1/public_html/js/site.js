85635bbfa91ffd02c4a76f8f43098075b9fea3c4
Gaspar Zaragoza <gaspar.zaragoza@globant.com>
2013-11-06 17:57:51 -0300

    Renaming some folders :)

--- Nivel 1/public_html/js/site.js
+++ Nivel 1/public_html/js/site.js
@@ -4,15 +4,15 @@ $(function() {
 
 function activateLinks() {
     $("a").not(".item-menu").click(function( event ) {
-        event.preventDefault();
         if($(this).attr("href")!="#"){
+            event.preventDefault();
             $.ajax( "/obj/"+$(this).attr("href")+".html" )
                 .done(function(data) {
                     $("#main_content").empty();
                     $("#main_content").append(data);
                     activateLinks();
             });
+            return false;
         }
-        return false;
     });
 }
