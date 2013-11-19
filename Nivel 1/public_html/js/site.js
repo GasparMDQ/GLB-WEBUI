fd1b92558beea6b570a1f93748e54bc0055a35c1
Gaspar Zaragoza <gaspar.zaragoza@globant.com>
2013-11-19 15:58:14 -0300

    MinFix @routes

--- Nivel 1/public_html/js/site.js
+++ Nivel 1/public_html/js/site.js
@@ -6,7 +6,7 @@ function activateLinks() {
     $("a").not(".item-menu").click(function( event ) {
         if($(this).attr("href")!="#"){
             event.preventDefault();
-            $.ajax( "/obj/"+$(this).attr("href")+".html" )
+            $.ajax( "obj/"+$(this).attr("href")+".html" )
                 .done(function(data) {
                     $("#main_content").empty();
                     $("#main_content").append(data);
