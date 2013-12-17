8bc9df2c6f00f2a13bdf628c7ce0d5190087e988
Gaspar Zaragoza <gaspar.zaragoza@globant.com>
2013-12-17 11:52:18 -0300

    Base para el modulo 1 del nivel 3 (not working yet)

---
+++ Nivel 3/Modulo 1/public_html/js/app.js
@@ -0,0 +1,168 @@
+"use strict";
+
+require.config({
+  paths: {
+    'underscore': 'lib/underscore-min',
+    'jquery': 'lib/jquery-1.10.2.min',
+    'backbone': 'lib/backbone-min',
+    'localstorage': 'lib/backbone.localStorage-min',
+    'jquery.bootstrap': 'lib/bootstrap.min'
+  },  
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: ['underscore', 'jquery'],
+      exports: 'Backbone'
+    },
+    'jquery.bootstrap': {
+        deps: ['jquery']
+    }
+  }
+});
+
+require(['jquery', 'underscore', 'backbone', 'localstorage', 'jquery.bootstrap'], function($, _, Backbone){
+  
+  var Product = Backbone.Model.extend({
+    defaults: function () {
+      return {
+        id: '',
+        name: '',
+        precio: '',
+        descripcion: '',
+        image: ''
+      };
+    }
+  });
+  
+  var Warehouse = Backbone.Collection.extend({
+    localStorage: new Backbone.LocalStorage('warehouse'),
+    model: Product
+  });
+  
+  var Item = Backbone.Model.extend({
+    defaults: function(){
+      return {
+        id: '',
+        product_id: '',
+        cantidad: ''
+      };
+    }
+  });
+  
+  var Lineadecaja = Backbone.Collection.extend({
+    localStorage: new Backbone.LocalStorage('lineadecaja'),
+    model: Item
+  });
+
+  
+  var WareHouseView = Backbone.View.extend({
+    el: $('body'),
+    template: _.template($('#products-list-tmpl').html()),
+    model: Product,
+    events: {
+      'click .btn-details': 'showDetails'
+    },
+    
+    initialize: function () {
+      _.bindAll(this, 'render', 'showDetails');
+      this.render();
+    },
+
+    render: function () {
+      WareHouse.fetch();
+      WareHouse.comparator = "name";
+      WareHouse.sort();
+      $('#products-list > ul').html(this.template({
+        products: WareHouse.toJSON()
+      }));
+      return this;
+    },
+
+    showDetails: function (ev) {
+      $('#contact-detail-id').attr('value', $(ev.currentTarget).attr('value'));
+      var DetailView = new PhoneBookDetailView();
+      DetailView.render();
+    },
+    
+  });
+  
+  var NewProductView = Backbone.View.extend({
+    el: $('#products-list'),
+    template: _.template($('#products-new-tmpl').html()),
+    model: Product,
+    events: {
+      'click .btn-add': 'guardar'
+    },
+    
+    initialize: function () {
+      _.bindAll(this, 'render', 'guardar');
+      this.render();
+    },
+
+    render: function () {
+      this.$el.html(this.template());
+      return this;
+    },
+
+    guardar: function (ev) {
+      $('#contact-detail-id').attr('value', $(ev.currentTarget).attr('value'));
+      var DetailView = new PhoneBookDetailView();
+      DetailView.render();
+    }
+    
+  });
+  
+  var PhoneBookDetailView = Backbone.View.extend({
+    el: $('#products-details'),
+    template: _.template($('#products-details-tmpl').html()),
+    model: Product,
+
+    initialize: function () {
+      _.bindAll(this, "render");
+    },
+    
+    render: function () {
+      /*
+      var cId = $('#contact-detail-id').val();
+      if (cId !== '0') {
+        var eContact = PhoneBook.get(cId);
+        this.$el.html(this.template({
+          contact: eContact.toJSON()
+        }));
+      } else {
+        this.$el.empty();
+      }
+      return this;
+    */
+    }
+  });
+  
+  var WareHouse = new Warehouse();
+  var LineaDeCaja = new Lineadecaja();
+  //var App = new WareHouseView();
+  
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      '': 'warehouseview',
+      'list': 'warehouseview',
+      'checkout': 'checkout',
+      'newProduct': 'newP'
+    },
+    
+    warehouseview: function(){
+      new WareHouseView();
+    },
+    checkout: function(){
+      //new CheckOutView();
+    },
+    newP: function(){
+      new NewProductView();
+    }
+  });
+  
+  var router = new AppRouter();
+  Backbone.history.start();
+
+});
